d1b914b05daf5ac0419c37f5b1430fe4cf4476f2
Ouadie <elamraniouadie@gmail.com>
2025-06-23 23:48:43 +0200

    Queue Implementation: Adding some example

--- queue.js
+++ queue.js
@@ -23,10 +23,23 @@ function Queue(){
     this.front = function(){
        if(collection.length > 0){
           return collection[0];
-          }else{return null;}
+          }else{
+            return null;
+        };
     };
     
     this.isEmpty = function(){
        return (collection.length === 0);
     };
 }
+
+var q = new Queue();
+q.enqueue("John");
+q.enqueue("Paul");
+q.enqueue("Jane");
+console.log(q.size()); // 3
+console.log(q.front()); // John
+q.print(); // [ 'John', 'Paul', 'Jane' ]
+q.dequeue(); 
+console.log(q.front()); // Paul
+q.print(); // [ 'Paul', 'Jane' ]
