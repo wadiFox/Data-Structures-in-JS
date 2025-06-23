e9942fd067c5ce80cc2a16d990dbcf58d91850d2
Ouadie <elamraniouadie@gmail.com>
2025-06-24 00:34:36 +0200

    Queue Implementation: Adding Queue With Priority

--- queue.js
+++ queue.js
@@ -43,3 +43,67 @@ q.print(); // [ 'John', 'Paul', 'Jane' ]
 q.dequeue(); 
 console.log(q.front()); // Paul
 q.print(); // [ 'Paul', 'Jane' ]
+
+// Queue with priority
+function priorityQueue(){
+    collection = [];
+    
+    this.print = function(){
+        console.log(collection);
+        }
+    
+    this.enqueue = function(item){
+        if(this.isEmpty()){
+            collection.push(item);
+        }else{
+            var added = false;
+            for(var i = 0; i < collection.length; i++){
+                if(item[1] < collection[i][1]){// checking priorities
+                collection.splice(i, 0, item);
+                added = true;
+                break;
+                }
+            }
+        if(!added){
+        collection.push(item);
+        }
+     }
+ }
+    
+    this.dequeue = function(){
+       var value = collection.shift();
+       return value[0];
+    } 
+    
+    this.front = function(){
+        return collection[0];
+    }
+
+    this.size = function(){
+        return collection.length;
+    }
+
+    this.isEmpty = function(){
+        return collection.length === 0;
+    }
+    
+    }    
+
+     var q = new priorityQueue(); 
+   
+   q.enqueue(["John Doe", 3]);
+   q.enqueue(["John Smith", 2]);
+   q.enqueue(["Jane Doe", 1]);
+   q.enqueue(["Greg Davis", 4]);
+   q.enqueue(["Georges Lukas", 5]);
+   q.print();
+   console.log(q.size());
+   console.log(q.front());
+   q.dequeue();
+   q.print();
+   console.log(q.size());
+   console.log(q.front());
+   q.dequeue();
+   q.print();
+   console.log(q.size());
+   console.log(q.front());
